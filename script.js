ecb208f96dfc138ca45d6a7d901c4bcc22ec0e96
De vona C <devonacanada@gmail.com>
2024-09-05 12:12:52 -0400

    starter code added

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
-const addEmployeesBtn = document.querySelector(`#add-employees-btn`);
-const employeesList = document.querySelector(`#employees-list`);
-const employees = [];
+//STARTER CODE
+//Do not modify any of the code below this line:
+
+
+// Display employee data in an HTML table
+const displayEmployees = function (employeesArray) {
+// Get the employee table
+const employeeTable = document.querySelector('#employee-table');
+
+// Clear the employee table
+employeeTable.innerHTML = '';
+
+// Loop through the employee data and create a row for each employee
+for (let i = 0; i < employeesArray.length; i++) {
+  const currentEmployee = employeesArray[i];
+
+  const newTableRow = document.createElement('tr');
+
+  const firstNameCell = document.createElement('td');
+  firstNameCell.textContent = currentEmployee.firstName;
+  newTableRow.append(firstNameCell);
+
+  const lastNameCell = document.createElement('td');
+  lastNameCell.textContent = currentEmployee.lastName;
+  newTableRow.append(lastNameCell);
+
+  const salaryCell = document.createElement('td');
+  // Format the salary as currency
+  salaryCell.textContent = currentEmployee.salary.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  newTableRow.append(salaryCell);
+
+  employeeTable.append(newTableRow);
+}
+};
+
+const trackEmployeeData = function () {
+const employees = collectEmployees();
+
+console.table(employees);
+
+displayAverageSalary(employees);
+
+console.log('==============================');
+
+getRandomEmployee(employees);
+
+employees.sort(function (a, b) {
+  if (a.lastName < b.lastName) {
+    return -1;
+  } else {
+    return 1;
+  }
+});
+
+displayEmployees(employees);
+};
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
+
+//Student code Below
